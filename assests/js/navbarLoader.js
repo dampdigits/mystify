586da7553dec6672d8216767b71c05624a4b0455
Sk Sameer Salam <113439449+its-triple-s@users.noreply.github.com>
2023-08-20 16:36:26 +0000

    moved assests into folder

--- assests/js/navbarLoader.js
+++ assests/js/navbarLoader.js
@@ -1,7 +1,7 @@
 // navbarLoader.js
 const loadNavbar = async () => {
     try {
-        const response = await fetch('navbar.html');
+        const response = await fetch('../../navbar.html');
         if (!response.ok) {
             throw new Error('Failed to fetch navbar content');
         }

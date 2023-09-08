c86b424f3af46fea279d31eeeea2ec2a192aa3dd
Sk Sameer Salam <113439449+its-triple-s@users.noreply.github.com>
2023-09-08 17:58:46 +0000

    carousel button color change

--- assets/js/switchTheme.js
+++ assets/js/switchTheme.js
@@ -1,46 +1,57 @@
 // theme.js
 
-// Select the button and image
+// Select theme button
 let button = document.querySelector('#themeBtn');
-let image = document.querySelector('#themeIcon');
+// Select theme button icon
+let theme_button_icon = document.querySelector('#themeIcon');
+// Get all carousel control buttons
+let buttons = document.querySelectorAll('.carousel-control-prev, .carousel-control-next');
 
 // Switch theme on button click
 button.onclick = function() {
-    // Find current theme
+    // Switch webpage theme
     let element = document.body;
     let theme = element.dataset.bsTheme === "light" ? "dark" : "light";
     element.dataset.bsTheme = theme;
 
-    let button_image = "";
-    let buttonPrev = document.querySelector('.carousel-control-prev');
-    let buttonNext = document.querySelector('.carousel-control-next');
-    
-    // Switch theme
+    // Switch button theme_button_icon
+    let button_icon = "";
     if (theme === "light") {
-        button_image = "assets/icons/themes/moon-stars-fill.svg";
-        buttonPrev.style.backgroundColor = "grey";
-        buttonNext.style.backgroundColor = "grey";
+        button_icon = "assets/icons/themes/moon-stars-fill.svg";
     } else {
-        button_image = "assets/icons/themes/sun-fill.svg";
-        buttonPrev.style.backgroundColor = "white";
-        buttonNext.style.backgroundColor = "white";
+        button_icon = "assets/icons/themes/sun-fill.svg";
     }
-    image.setAttribute('src', button_image);
-    // save theme preference when user switches theme
+    theme_button_icon.setAttribute('src', button_icon);
+
+    let carousel_button_color = theme === "light" ? "grey" : "white";
+    // Change color of carousel buttons
+    buttons.forEach(function(carouselButton) {
+        carouselButton.style.backgroundColor = carousel_button_color;
+    });
+
+    // Save theme preference when user switches theme
     localStorage.setItem('theme', theme);
-    localStorage.setItem('button_image', button_image);
+    localStorage.setItem('theme_button_icon', button_icon);
+    localStorage.setItem('carousel_button_color',carousel_button_color);
 };
 
 // Check for saved theme preference on page load
 document.addEventListener('DOMContentLoaded', function() {
     let theme = localStorage.getItem('theme');
-    let button_image = localStorage.getItem('button_image');
+    let button_icon = localStorage.getItem('theme_button_icon');
+    let carousel_button_color = localStorage.getItem('carousel_button_color');
+
     // Change to preferenced theme
     if (theme != null) {
         let element = document.body;
         element.dataset.bsTheme = theme;
     }
-    if (button_image != null) {
-        image.setAttribute('src', button_image);
+    if (button_icon != null) {
+        theme_button_icon.setAttribute('src', button_icon);
+    }
+    if (carousel_button_color != null) {
+        buttons.forEach(function(carouselButton) {
+            carouselButton.style.backgroundColor = carousel_button_color;
+        });
     }
 });
